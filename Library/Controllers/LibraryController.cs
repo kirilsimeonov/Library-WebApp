d53402cf890ddd72c9f0485917d502fad75617d6
Kiril Simeonov <k.s.simeonov@gmail.com>
2019-04-06 11:44:23 +0300

    Final Version of Library Web App

---
+++ Library/Controllers/LibraryController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Library.Models;
+using Library.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class LibraryController : Controller
+    {
+        public IActionResult Index()
+        {
+            using (var db = new LibraryDb ())
+            {
+                var tasks = db.Books.ToList();
+                return View(tasks);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(string title, string author, double price)
+        {
+            if (string.IsNullOrEmpty(title) || string.IsNullOrEmpty(author) || price<1)
+            {
+                return RedirectToAction("Index");
+            }
+            using (var db = new LibraryDb())
+            {
+                Book book = new Book
+                {
+                    Title = title,
+                    Author = author,
+                    Price = price
+                
+                };
+
+                db.Books.Add(book);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            using (var db = new LibraryDb())
+            {
+                var books = db.Books.Find(id);
+
+                return View(books);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Book book)
+        {
+
+            using (var db = new LibraryDb())
+            {
+                db.Books.Update(book);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            using (var db = new LibraryDb())
+            {
+                var tasks = db.Books.Find(id);
+
+                return View(tasks);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Book book)
+        {
+            using (var db = new LibraryDb())
+            {
+                db.Books.Remove(book);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Library/Data/LibraryDb.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Library.Models;
+using Library.Controllers;
+
+namespace Library.Data
+{
+    public class LibraryDb : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+
+        private const string ConnectionString = @"Server=.\SQLEXPRESS;Database=LibraryDb;Integrated Security=True;";
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(ConnectionString);
+        }
+    }
+}
+
